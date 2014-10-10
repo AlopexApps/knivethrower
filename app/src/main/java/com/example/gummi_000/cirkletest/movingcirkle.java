0bcede3a270d89faf23a698750b43270a1b97f17
unknown <gummisigurjons@gmail.com>
2014-10-10 11:06:18 -0700

    add files

---
+++ app/src/main/java/com/example/gummi_000/cirkletest/movingcirkle.java
@@ -0,0 +1,40 @@
+package com.example.gummi_000.cirkletest;
+
+import android.app.Activity;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class movingcirkle extends Activity {
+
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new Circle(this, 50, 50, 25));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.movingcirkle, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
